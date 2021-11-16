ea17b77a779bf23a7fc1a77d1b90d4eecc3e5a2d
hsuh21 <89887868+hsuh21@users.noreply.github.com>
2021-11-16 00:10:21 -0500

    homepage

--- java/homespage.js
+++ java/homespage.js
@@ -1,4 +1,13 @@
-function ani() {
+window.addEventListener("load", function() {
+  document.getElementById("doorbell").addEventListener("click", answerDoor);
+
+  document.getElementById("doorbell").addEventListener("animationend", spinEnd);
+
+  function answerDoor() {
     document.getElementById('doorbell').className = 'classname';
-    
   }
+  
+  function spinEnd() {
+    window.location.href = 'index.html';
+  }
+})
